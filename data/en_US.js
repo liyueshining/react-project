85521e941729e0d2ce6fc9c8c8d320993b2197c7
Yue.Lee <liyueshining@gmail.com>
2017-05-16 20:12:32 +0800

    Create en_US.js

---
+++ data/en_US.js
@@ -0,0 +1,24 @@
+import DATA from './Protocals_en';
+
+const en_US = {
+    name: 'en',
+    languageToConverter: '中文',
+    permissionTitle: 'Software and Service Permission',
+    permissionContent: DATA.PROTOCALS,
+    acceptButton: 'Accept',
+
+    'installPage.tile': 'U Install',
+    'installPage.vMAddress': 'vM Address：',
+    'installPage.Location': 'Location：',
+    'installPage.placeholder': 'please input vM address',
+    'installPage.chooseFile': 'Choose File',
+    'installPage.choosedFileName': 'Name',
+    'installPage.choosedFileSize': 'Size',
+    'installPage.fileOperations': 'Operations',
+    'installPage.upload': 'Upload',
+    'installPage.delete': 'Delete',
+    'installPage.uploadProgress': 'Result: ',
+    'installPage.install': 'Install'
+}
+
+export default en_US
